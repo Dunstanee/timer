2f1db910e39a536766a9509127666f73cf42fd76
dunstana <dunstan@afyarekod.com>
2025-02-16 10:20:18 +0300

    updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,53 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+import React, { useEffect, useRef, useState } from "react";
+// import css from public
+import "./globals.css";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+  const [time, setTime] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+  const interval = useRef<number | null>(null);
+
+  useEffect(() => {
+    if (isRunning) {
+   interval.current = window.setInterval(() => {
+        setTime((prevTime) => prevTime + 1);
+      }, 1000);
+      
+    } else {
+      if (interval.current) {
+        window.clearInterval(interval.current);
+      }
+    }
+  }, [isRunning]);
+ 
+  const handleStart = () => {
+    console.log("start");
+    setIsRunning(true);
+  };
+  const handleStop = () => {
+    console.log("stop");
+    setIsRunning(false);
+  };
+  const handleReset = () => {
+    setTime(0);
+  };
+
+  return (
+   <div className="content d-flex align-items-center justify-content-center h-100">
+    <div className="card col-xxl-4">
+      <div className="card-body">
+        <h1 className="card-title">{time}</h1>
+      </div>
+      <div className="card-footer">
+        <div className="d-flex justify-content-between">
+          <button type="button"  onClick={handleStop} className="btn btn-sm btn-outline-danger me-3">Stop</button>
+          <button type="button" onClick={handleStart} className="btn btn-sm btn-success">Start</button>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
     </div>
+    
+   </div>
   );
 }
